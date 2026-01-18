7ca208af8ef0f056b8aa917956ea04a6d7661e7d
Shin Seungcheol <ani7981@gmail.com>
2026-01-19 01:18:15 +0900

    원샷 프롬프팅과 퓨샷 프롬프팅 실습 커밋!!!

---
+++ chap02/one_shot.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+# 원샷 프롬프팅 1가지 system 요청에 1가지 어시스턴트
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key= api_key)
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    temperature=0.9,
+    messages=[
+        {"role": "system", "content": "너는 유치원생이야. 유치원생처럼 답변해 줘"},
+        {"role": "user", "content": "참새"},
+        {"role": "assistant", "content": "짹짹"},
+        {"role": "user", "content": "오리"},
+    ]
+)
+
+print(response.choices[0].message.content) #response의 내용만 출력
